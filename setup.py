38f51d15766ead70402036e882ab748e7f71ea00
JM-Xia <111189278+JM-Xia@users.noreply.github.com>
2024-12-06 21:20:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="agglutination_calculator",
+    name="agglutinationconcentraioncalculator",
     version="0.1.0",
     author="Gabrielle",
     description="A tool for analyzing agglutination patterns and calculating concentrations",
